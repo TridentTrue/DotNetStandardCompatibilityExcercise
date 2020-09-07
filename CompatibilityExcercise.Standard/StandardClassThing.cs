6f4d084650fab76207ddbd5f70ed4504d4560224
Adam Donaldson <adam.donaldson@nihe.gov.uk>
2020-09-07 14:06:55 +0100

    Add project files.

---
+++ CompatibilityExcercise.Standard/StandardClassThing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CompatibilityExcercise.Standard
+{
+	public class StandardClassThing
+	{
+		public int Num1 { get; set; }
+		public int Num2 { get; set; }
+
+		public StandardClassThing(int num1, int num2)
+		{
+			this.Num1 = num1;
+			this.Num2 = num2;
+		}
+
+		public int Calculate() => Num1 + Num2;
+	}
+}
